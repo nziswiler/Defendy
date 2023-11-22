aeb872bf1cdf8895d2956dd9005ce19bfb86a0f3
nziswiler <nino1999@bluewin.ch>
2023-11-22 23:36:19 +0000

    Fix packet version

--- config/wazuh-agent/linux/generate-linux-agent-installer.sh
+++ config/wazuh-agent/linux/generate-linux-agent-installer.sh
@@ -18,7 +18,7 @@ if [ -f "/etc/redhat-release" ]; then
     curl -L -O https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-8.11.1-x86_64.rpm && sudo rpm -vi packetbeat-8.11.1-x86_64.rpm
 elif [ -f "/etc/debian_version" ]; then
     wget https://packages.wazuh.com/4.x/apt/pool/main/w/wazuh-agent/wazuh-agent_4.6.0-1_amd64.deb && sudo WAZUH_MANAGER='$SERVER' WAZUH_MANAGER_PORT='$WAZUH_AGENT_CONNECTION_PORT' WAZUH_REGISTRATION_PORT='$WAZUH_AGENT_ENROLLMENT_PORT' WAZUH_AGENT_GROUP='Linux' dpkg -i wazuh-agent_4.6.0-1_amd64.deb
-    curl -L -O https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-7.16.3-amd64.deb && sudo dpkg -i packetbeat-8.11.1-amd64.deb
+    curl -L -O https://artifacts.elastic.co/downloads/beats/packetbeat/packetbeat-8.11.1-amd64.deb && sudo dpkg -i packetbeat-8.11.1-amd64.deb
 else
     echo "Unknown distribution or unsupported package type."
     exit 1
