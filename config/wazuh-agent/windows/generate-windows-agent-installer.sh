3e912da0ed4b62b0031b412571a2059447e38a98
nziswiler <nino1999@bluewin.ch>
2023-11-23 08:44:43 +0000

    Fix generator script

--- config/wazuh-agent/windows/generate-windows-agent-installer.sh
+++ config/wazuh-agent/windows/generate-windows-agent-installer.sh
@@ -5,7 +5,7 @@ cd "$SCRIPT_DIR" || exit
 
 source ../../../.env
 SCRIPT_NAME="../../../install-windows-agent.ps1"
-SYSMON_CONFIG="https://raw.githubusercontent.com/nziswiler/Defendy/main/config/wazuh-agent/linux/packetbeat.yml"
+SYSMON_CONFIG="https://raw.githubusercontent.com/nziswiler/Defendy/main/config/wazuh-agent/windows/sysmonconfig.xml"
 
 # Based on: https://gist.githubusercontent.com/taylorwalton/22b3ef3f624edd494ebf640aba56120c/raw/cee2383e91aea2b850bd87c118f20fc010955cf8/sysmon_install.ps1
 cat > "$SCRIPT_NAME" <<EOF
@@ -27,50 +27,32 @@ NET START WazuhSvc
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
-if (Test-Path -Path \$sysinternals_folder) {
-    write-host ('Sysinternals folder already exists')
-} else {
-    \$OutPath = \$env:TMP
-    \$output = \$sysinternals_zip
-    New-Item -Path "C:\Program Files" -Name "sysinternals" -ItemType "directory"
-    \$X = 0
-    do {
-        Write-Output "Waiting for network"
-        Start-Sleep -s 5
-        \$X += 1
-    } until((\$connectreult = Test-NetConnection \$sysinternals_repo -Port 443 | ? { $_.TcpTestSucceeded }) -or \$X -eq 3)
-
-    if (\$connectreult.TcpTestSucceeded -eq \$true){
-        Try
-        {
-            write-host ('Downloading and copying Sysinternals Tools to C:\Program Files\sysinternals...')
-            Invoke-WebRequest -Uri \$sysinternals_downloadlink -OutFile \$OutPath\$output
-            Expand-Archive -path \$OutPath\$output -destinationpath \$sysinternals_folder
-            Start-Sleep -s 10
-            Invoke-WebRequest -Uri \$sysmonconfig_downloadlink -OutFile \$OutPath\$sysmonconfig_file
-            \$serviceName = 'Sysmon64'
-            If (Get-Service \$serviceName -ErrorAction SilentlyContinue) {
-                write-host ('Sysmon Is Already Installed')
-            } else {
-                Invoke-Command {reg.exe ADD HKCU\Software\Sysinternals /v EulaAccepted /t REG_DWORD /d 1 /f}
-                Invoke-Command {reg.exe ADD HKU\.DEFAULT\Software\Sysinternals /v EulaAccepted /t REG_DWORD /d 1 /f}
-                Start-Process -FilePath \$sysinternals_folder\Sysmon64.exe -Argumentlist @("-i", "\$OutPath\$sysmonconfig_file")
-            }
-        }
-        Catch
-        {
-            \$ErrorMessage = $_.Exception.Message
-            \$FailedItem = $_.Exception.ItemName
-            Write-Error -Message "\$ErrorMessage \$FailedItem"
-            exit 1
-        }
-        Finally
-        {
-            Remove-Item -Path \$OutPath\$output
-        }
+Try
+{
+    write-host ('Downloading and copying Sysinternals Tools to C:\Program Files\sysinternals...')
+    Invoke-WebRequest -Uri \$sysinternals_downloadlink -OutFile \$OutPath\$output
+    Expand-Archive -path \$OutPath\$output -destinationpath \$sysinternals_folder
+    Start-Sleep -s 10
+    Invoke-WebRequest -Uri \$sysmonconfig_downloadlink -OutFile \$OutPath\$sysmonconfig_file
+    \$serviceName = 'Sysmon64'
+    If (Get-Service \$serviceName -ErrorAction SilentlyContinue) {
+        write-host ('Sysmon Is Already Installed')
     } else {
-        Write-Output "Unable to connect to Sysinternals Repo"
+        Invoke-Command {reg.exe ADD HKCU\Software\Sysinternals /v EulaAccepted /t REG_DWORD /d 1 /f}
+        Invoke-Command {reg.exe ADD HKU\.DEFAULT\Software\Sysinternals /v EulaAccepted /t REG_DWORD /d 1 /f}
+        Start-Process -FilePath \$sysinternals_folder\Sysmon64.exe -Argumentlist @("-i", "\$OutPath\$sysmonconfig_file")
     }
+}
+Catch
+{
+    \$ErrorMessage = $_.Exception.Message
+    \$FailedItem = $_.Exception.ItemName
+    Write-Error -Message "\$ErrorMessage \$FailedItem"
+    exit 1
+}
+Finally
+{
+    Remove-Item -Path \$OutPath\$output
 }
 EOF
 
